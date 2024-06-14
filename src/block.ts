96f0a2b7994eba81ed99b6c3929222d7e935065f
Naomi <commits@nhcarrigan.com>
2024-06-14 11:05:13 -0700

    feat: add cli command to block emails

---
+++ src/block.ts
@@ -0,0 +1,31 @@
+import { MongoClient } from "mongodb";
+
+import { asyncExec } from "./utils/asyncExec";
+import { logHandler } from "./utils/logHandler";
+
+(async () => {
+  const email = process.argv[2];
+  await asyncExec(
+    `aws sesv2 put-suppressed-destination --reason COMPLAINT --email-address ${email}`
+  );
+  logHandler.info("Added to AWS suppression list.");
+  if (!process.env.MONGO_URI) {
+    logHandler.error("No mongo url in env!");
+    return;
+  }
+  const mongo = new MongoClient(process.env.MONGO_URI);
+  const db = mongo.db("freecodecamp");
+  const collection = db.collection("user");
+  await collection.findOneAndUpdate(
+    {
+      email,
+    },
+    {
+      $set: {
+        sendQuincyEmail: false,
+      },
+    }
+  );
+  logHandler.info("Unsubscribed from newsletter.");
+  await mongo.close();
+})();
